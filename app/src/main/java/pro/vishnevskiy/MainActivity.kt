6f36fabfdefcd0cf40ad18fcf31ccca7b8e464fa
Oleg Vishnevskiy <vishnevskiyoleg@gmail.com>
2023-05-21 14:48:11 +0300

    release version

--- app/src/main/java/pro/vishnevskiy/MainActivity.kt
+++ app/src/main/java/pro/vishnevskiy/MainActivity.kt
@@ -34,7 +34,6 @@ class MainActivity : AppCompatActivity() {
         binding.beamLayout.setOnClickListener {
             //In case on click layout
             collectStatistic.pushDataToFirebaseOnClickLayout()
-
         }
     }
 
